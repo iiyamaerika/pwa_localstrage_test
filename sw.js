2ca3a11509ab96a3295ed0a98d6f19b8e5997b8e
vitals\er-iiyama <er-iiyama@vitals.co.jp>
2023-11-15 14:29:17 +0900

    commit test

---
+++ sw.js
@@ -0,0 +1,28 @@
+// PWA対応：Service Worker
+
+var CACHE_NAME = 'color-name';
+var urlsToCache = [
+    '/localstrage-test/index.js'
+];
+
+// インストール処理
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+// リソースフェッチ時のキャッシュロード処理
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches
+            .match(event.request)
+            .then(function(response) {
+                return response || fetch(event.request);
+            })
+    );
+});
